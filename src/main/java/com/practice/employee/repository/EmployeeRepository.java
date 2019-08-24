beb02ec3688afec15cd9fc2d470d07a191846583
Gopal Krushna Pattanaik <gopalkrushna.pattanaik@tesco.com>
2019-08-24 23:44:13 +0530

    refactored

--- src/main/java/com/practice/employee/repository/EmployeeRepository.java
+++ src/main/java/com/practice/employee/repository/EmployeeRepository.java
@@ -8,10 +8,13 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.practice.employee.pojo.Employee;
 
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
-	
+
 	public Optional<Employee> findById(String employeeId);
-    public List<Employee> findByDepartment(String department);
-    public Employee save(Employee employee);
-    public List<Employee> findAll();
+
+	public List<Employee> findByDepartment(String department);
+
+	public Employee save(Employee employee);
+
+	public List<Employee> findAll();
 
 }
